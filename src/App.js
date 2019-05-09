ec1a2503a66a0f02219af9d2a32995ad18120a28
Jakub Przybysz <Jakub.Przybysz@tlx.local>
2019-05-09 11:30:38 +0200

    filtrowana lista

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // z braku laku istniaj¹cych userów definiujê po prostu jako sta³¹
+    // w prawdziwej aplikacji pochodziliby z API i/lub byli w Reduksowym storze 
+    const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
+
+    class App extends React.Component {
+        constructor() {
+            super();    
+            
+            this.state = {
+            filteredUsers: allUsers
+            };
+        }
+
+        filterUsers(e) {
+            const text = e.currentTarget.value;
+            const filteredUsers = this.getFilteredUsersForText(text)
+            this.setState({
+            filteredUsers
+            })
+        }
+        
+        getFilteredUsersForText(text) {
+            return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+        }
+        
+        render () {
+            return (
+                <div>
+                    <input onInput={this.filterUsers.bind(this)} />
+                    <UsersList users={this.state.filteredUsers} />
+                </div>
+            );
+        }
+    };
+
+    const UsersList = ({ users }) => {
+        if (users.length > 0) {
+            return (
+            <ul>
+                {users.map(user => <li key={user}>{user}</li>)}
+            </ul>
+            );
+        }
+
+        return (
+            <p>No results!</p>
+        );
+    };
 
 export default App;
