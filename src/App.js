52107ca017f83c2d56163f12398be797148c8338
Jakub Przybysz <Jakub.Przybysz@tlx.local>
2019-05-13 09:19:39 +0200

    Enzyme part 1

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
-import React from 'react';
-import './App.css';
+import * as React from 'react';
+import UsersList from './UsersList'
 
-    // z braku laku istniaj¹cych userów definiujê po prostu jako sta³¹
-    // w prawdziwej aplikacji pochodziliby z API i/lub byli w Reduksowym storze 
-    const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
+// z braku laku istniaj¹cych userów definiujê po prostu jako sta³¹
+// w prawdziwej aplikacji pochodziliby z API i/lub byli w Reduksowym storze 
+const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
 
-    class App extends React.Component {
-        constructor() {
-            super();    
-            
-            this.state = {
-            filteredUsers: allUsers
-            };
-        }
+class App extends React.Component {
+  constructor() {
+    super();
 
-        filterUsers(e) {
-            const text = e.currentTarget.value;
-            const filteredUsers = this.getFilteredUsersForText(text)
-            this.setState({
-            filteredUsers
-            })
-        }
-        
-        getFilteredUsersForText(text) {
-            return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
-        }
-        
-        render () {
-            return (
-                <div>
-                    <input onInput={this.filterUsers.bind(this)} />
-                    <UsersList users={this.state.filteredUsers} />
-                </div>
-            );
-        }
+    this.state = {
+      filteredUsers: allUsers
     };
+  }
 
-    const UsersList = ({ users }) => {
-        if (users.length > 0) {
-            return (
-            <ul>
-                {users.map(user => <li key={user}>{user}</li>)}
-            </ul>
-            );
-        }
+  filterUsers = (e) => {
+    const text = e.currentTarget.value;
+    const filteredUsers = this.getFilteredUsersForText(text)
+    this.setState({
+      filteredUsers
+    })
+  }
 
-        return (
-            <p>No results!</p>
-        );
-    };
+  getFilteredUsersForText(text) {
+    return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+  }
+
+  render() {
+    return (
+      <div>
+        <input onInput={this.filterUsers} />
+        <UsersList users={this.state.filteredUsers} />
+      </div>
+    );
+  }
+};
 
 export default App;
